89f6e1f0690fe2d64d83779850c54f416c047ca8
kauramanp <33446281+kauramanp@users.noreply.github.com>
2024-08-30 02:49:19 +0530

    Initial commit

---
+++ toasty/src/main/java/com/aman/toasty/Toaster.kt
@@ -0,0 +1,10 @@
+package com.aman.toasty
+
+import android.content.Context
+import android.widget.Toast
+
+object Toaster {
+    fun simpleToast(context: Context?, msg: String?) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+    }
+}
